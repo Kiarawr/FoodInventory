20e8074838407de5eeee1711f1120dc537c3e7a8
Kiara Chong <kdchong@sympatico.ca>
2021-07-16 04:20:38 -0400

    modified to render after getting data

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,51 +1,67 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { View, StyleSheet} from 'react-native';
+import { Icon } from 'react-native-elements';
 import { AuthContext } from '../navigation/AuthProvider';
 import { Calendar, Text, Layout, Button } from '@ui-kitten/components';
 import firestore from '@react-native-firebase/firestore';
 import { firebase } from '@react-native-firebase/firestore';
+import { useIsFocused } from '@react-navigation/native';
 
+function HomeScreen(props) {
 
-function HomeScreen() {
   const { logout } = useContext(AuthContext);
   const [date, setDate] = React.useState(new Date());
   const [shopDates, setShopDates] = useState([]);
-  const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
+
+  const [isLoading, setLoading] = React.useState(true);
+  // const isFocused = useIsFocused();
+
+  useEffect(async () => {
     const currentUser = firebase.auth().currentUser;
-    let temp = []
-    const users = firestore().collection('users').doc(currentUser.uid).get()
-    .then((user) => {user.data()['items'].forEach(
+    let temp = [];
+    await firestore().collection('users').doc(currentUser.uid)
+    .get().then(user => {user.data()['items'].forEach(
       item => {
         let day = new Date (item.next_purchase_date._seconds * 1000).getTime();
         temp.push(day);
         setShopDates(temp);
       }
     )});
+
+    setLoading(false);
+
   }, []);
 
   const DayCell  = ({ date }, style ) => (
+
     <View
       style={[styles.dayContainer, style.container]}> 
       <Text style={styles.text}>{`${date.getDate()}`}</Text>
   
       <Text>
-      { shopDates.includes(date.getTime()) ? <Text style={[style.text, styles.value]}> yes </Text> : <Text> no </Text>  }
+        { shopDates.includes(date.getTime()) ? 
+        <Icon name = "ellipse" type = "ionicon" size = {5} color = "#7C4AF0"></Icon> : 
+        <View></View>}
       </Text>
+
     </View>
     
   );
 
   return (
     <React.Fragment>
+    {isLoading ? 
+      
+      <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'flex-start'}}>
+      </Layout>
+      
+      : 
+    
       <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'flex-start'}}>
         
         <Text category='h1' style={{marginLeft: 40}}>
               Overview
         </Text>
-
-        
 
         <Calendar
           style={{alignSelf: 'center'}}
@@ -61,7 +77,10 @@ function HomeScreen() {
         </Button>
 
       </Layout>
+    
+    }
     </React.Fragment>
+
   );
 };
 
